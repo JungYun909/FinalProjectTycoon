3a5e9ccef1288e0e5705db82167bd0398fe1079a
kim-soo-ah <149459760+kim-soo-ah@users.noreply.github.com>
2024-01-22 19:57:45 +0900

    [ADD, FIX] Tilemap Add, Script Fix
    타일맵 추가 moveBtn스크립트 수정

--- Assets/02. Scripts/02. GameObjects/InteractionObjects/UIs/Btns/MoveBtn.cs
+++ Assets/02. Scripts/02. GameObjects/InteractionObjects/UIs/Btns/MoveBtn.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class MoveBtn : InteractionData
 {
+    public Tilemap tilemap;
     public override void InitSetting()
     {
         _interactionStat.name = "Move Button";
@@ -14,7 +16,7 @@ public class MoveBtn : InteractionData
 
     public override void OnInteract()
     {
-        Vector2 mouseDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mouseDirection = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
         gameObject.transform.parent.position = new Vector2(mouseDirection.x, mouseDirection.y + (gameObject.transform.parent.position.y - gameObject.transform.position.y));
         _interactionStat.curGameObject.transform.position = gameObject.transform.parent.position;
     }
