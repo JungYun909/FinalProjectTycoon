ca286b5f6c6fed0c8b4cd8473255970ebecd45df
angskyee <149351121+angskyee@users.noreply.github.com>
2024-01-26 17:42:13 +0900

    [FIX, ADD] Scripts
    npc 소환 소환매니저로

--- Assets/02. Scripts/00. Managers/CurrentlyNotInUse/DataManager.cs
+++ Assets/02. Scripts/00. Managers/CurrentlyNotInUse/DataManager.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour   // TODO 추후 데이터 저장 / 로딩 관리하기 위한 매니저. json
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    public static DataManager instance;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 }
--- Assets/02. Scripts/00. Managers/SpawnManager.cs
+++ Assets/02. Scripts/00. Managers/SpawnManager.cs
@@ -11,6 +11,10 @@ public class SpawnManager : MonoBehaviour
     [Header("StandardPrefab")]
     public GameObject installationObj;
     public GameObject ingredientObj;
+    public GameObject npcObj;
+
+    [Header("SpawnPosition")]
+    public GameObject door;
 
 
     public static SpawnManager instance;
@@ -18,6 +22,17 @@ public class SpawnManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnNPC());
+    }
+
+    private void Update()
+    {
+        if(StatManager.instance.curNpc < StatManager.instance.maxNpc)
+            StopCoroutine(SpawnNPC());
     }
 
     public void SpawnInstallaion(MachineSO installationData)
@@ -47,6 +62,25 @@ public class SpawnManager : MonoBehaviour
         UpdateObjTag(data.tag);
     }
 
+    IEnumerator SpawnNPC()
+    {
+        while (true)
+        {
+            float visitProbability = StatManager.instance.shopFame * 0.1f;
+        
+            int rand = UnityEngine.Random.Range(1, 100);
+
+            if (rand <= visitProbability)
+            {
+                GameObject curNPC =  PoolManager.instacne.SpawnFromPool(npcObj);
+                curNPC.transform.position = door.transform.position;
+                StatManager.instance.maxNpc += 1;
+            }
+            
+            yield return new WaitForSeconds(6 - (StatManager.instance.shopLevel / 20));
+        }
+    }
+    
     public void SpawnObjPositionSet(GameObject spawnObj, GameObject destinationObj)
     {
         spawnObj.transform.position = spawnObj.transform.position +
