56a0661bd5083db78b2c9a33d913360242aa7456
kim-soo-ah <149459760+kim-soo-ah@users.noreply.github.com>
2024-02-16 14:01:44 +0900

    [ADD] Script
    튜토리얼 진열대관련스크립트추가

---
+++ Assets/02. Scripts/05. Turorial/TutorialOpenInventory.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class TutorialOpenInventory : TutorialBase
+{
+    StandInventoryUI inventoryUI;
+
+    public override void Enter()
+    {
+        base.Enter();
+
+    }
+
+    public override void Execute(TutorialController tutorialController)
+    {
+        if (inventoryUI.playerInventory == true) tutorialController.SetNextTutorial();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
