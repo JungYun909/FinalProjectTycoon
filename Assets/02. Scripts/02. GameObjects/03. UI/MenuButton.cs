d066179520e36b8744fa586c589628a908572c57
angskyee <149351121+angskyee@users.noreply.github.com>
2024-02-20 18:33:12 +0900

    [ADD] Scripts
    버튼 컨트롤러 스크립트
    버튼 클릭시 비활성화 후 뒤로가기 버튼만 활성화
    클릭시 해당 버튼 효과 추가

--- Assets/02. Scripts/02. GameObjects/03. UI/MenuButton.cs
+++ Assets/02. Scripts/02. GameObjects/03. UI/MenuButton.cs
@@ -1,9 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.Rendering;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class MenuButton : MonoBehaviour
 {
+    [SerializeField] private List<Button> btns = new List<Button>();
+    [SerializeField] private float speed;
+    [SerializeField] private Button backBtn;
+
+    private GameObject usingBtn;
+    private RectTransform backBtnPos;
+    private List<RectTransform> btnsPos = new List<RectTransform>();
+
+    private bool OnBtn;
+    
+    private Vector2 backBtnDestination;
+    private Vector2 backBtnPosition;
+
+    private void Start()
+    {
+        backBtnPos = backBtn.GetComponent<RectTransform>();
+        backBtn.interactable = false;
+        
+        backBtnDestination = new Vector2(backBtnPos.anchoredPosition.x - 150, backBtnPos.anchoredPosition.y);
+        backBtnPosition = new Vector2(backBtnPos.anchoredPosition.x, backBtnPos.anchoredPosition.y);
+        
+        foreach (var btn in btns)
+        {
+            btnsPos.Add(btn.GetComponent<RectTransform>());
+        }
+    }
+
     public void OpenUI(UIBase openUI)
     {
         GameManager.instance.uiManager.OpenWindow(openUI);
@@ -16,6 +48,71 @@ public class MenuButton : MonoBehaviour
 
     public void BackBtn()
     {
+        OnBtn = false;
+        backBtn.interactable = false;
+        
         GameManager.instance.uiManager.CloseAll();
+        
+        foreach (var pos in btnsPos)
+        {
+            if (usingBtn != pos.gameObject)
+            {
+                Vector2 btnDestination = new Vector2(pos.anchoredPosition.x, pos.anchoredPosition.y + 150);
+                StartCoroutine(moveBtn(pos, btnDestination));
+            }
+        }
+        
+        StartCoroutine(moveBtn(backBtnPos, backBtnPosition));
+        
+        usingBtn.GetComponent<Image>().color = Color.white;
+        usingBtn = null;
+    }
+    
+    public void ButtonEffect(RectTransform curBtnPos)
+    {
+        OnBtn = true;
+        SetBtns();
+        
+        usingBtn = curBtnPos.gameObject;
+        usingBtn.GetComponent<Image>().color = Color.red;
+        
+        StartCoroutine(moveBtn(backBtnPos, backBtnDestination));
+        
+        foreach (var pos in btnsPos)
+        {
+            if (usingBtn != pos.gameObject)
+            {
+                Vector2 btnDestination = new Vector2(pos.anchoredPosition.x, pos.anchoredPosition.y - 150);
+                StartCoroutine(moveBtn(pos, btnDestination));
+            }
+        }
+    }
+
+    private IEnumerator moveBtn(RectTransform movePos, Vector2 desPos)
+    {
+        while (Vector2.Distance(movePos.anchoredPosition,desPos) > 20f)
+        {
+            movePos.anchoredPosition =
+                Vector2.Lerp(movePos.anchoredPosition, desPos, speed);
+
+            yield return null;
+        }
+
+        if (OnBtn)
+        {
+            backBtn.interactable = true;
+        }
+        else
+        {
+            SetBtns();
+        }
+    }
+    
+    private void SetBtns()
+    {
+        foreach (var btn in btns)
+        {
+            btn.interactable = !btn.interactable;
+        }
     }
 }
