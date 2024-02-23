0cf291abbb26a24942479fb921f7235ba3693a9c
kim-soo-ah <149459760+kim-soo-ah@users.noreply.github.com>
2024-02-23 13:18:47 +0900

    [ADD,FIX] Font, Sound
    폰트수정 음악수정 타이틀씬 소리설정추가

--- Assets/02. Scripts/05. Turorial/TutorialController.cs
+++ Assets/02. Scripts/05. Turorial/TutorialController.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class TutorialController : MonoBehaviour
 {
     [SerializeField] private List<TutorialBase> tutorials;
-    [SerializeField] private string nextSceneName = "";
+    //[SerializeField] private string nextSceneName = "";
 
     private TutorialBase curTutorial = null;
-    //public AudioClip tutoClearSound;
+    public AudioClip tutoClearSound;
 
     //private int curIndex = -1;
 
@@ -52,10 +52,11 @@ public class TutorialController : MonoBehaviour
 
         // 튜토리얼이 모두 끝난 후 넣을 코드 추가 작성
         Debug.Log("튜토리얼 끗!");
+        GameManager.instance.audioManager.PlaySFX(tutoClearSound);
 
-        if(!nextSceneName.Equals(""))
-        {
-            GameManager.instance.sceneManager.ChangeScene("SAScene2");
-        }
+        //if(!nextSceneName.Equals(""))
+        //{
+        //    GameManager.instance.sceneManager.ChangeScene("SAScene2");
+        //}
     }
 }
